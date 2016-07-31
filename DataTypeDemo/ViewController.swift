55f2d6d660f36a7ff09eb7d805fa7c418766db51
forwardto9 <forwarto9@hotmail.com>
2016-07-31 17:20:05 +0800

    add attribute string demo

--- DataTypeDemo/ViewController.swift
+++ DataTypeDemo/ViewController.swift
@@ -8,11 +8,48 @@
 
 import UIKit
 
+
+class MyDrawView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.whiteColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let attributeString1 = NSAttributedString(string: "we all have a grate dream. then we should take much time to achieve it")
+//        attributeString1.drawInRect(rect)
+        attributeString1.drawWithRect(rect, options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil)
+    }
+}
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let lable = UILabel(frame: CGRectMake(50, 50, 200, 20))
+        lable.backgroundColor = UIColor.grayColor()
+        view.addSubview(lable)
+        
+        
+        
+        let attributeString1 = NSAttributedString(string: "we all have a grate dream. then we should take much time to achieve it")
+        lable.attributedText = attributeString1;
+        
+        
+        
+        let drawView = MyDrawView(frame: CGRectMake(50, 150, 200, 150))
+        
+        self.view.addSubview(drawView)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +58,7 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
 }
 
