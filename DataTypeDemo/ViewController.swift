258370fa466dc4d337aad14d3768b1ca9e1234d5
forwardto9 <forwarto9@hotmail.com>
2016-08-01 19:17:31 +0800

    add UIImage

--- DataTypeDemo/ViewController.swift
+++ DataTypeDemo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreGraphics
 
 
 class MyDrawView: UIView {
@@ -23,7 +24,9 @@ class MyDrawView: UIView {
     override func drawRect(rect: CGRect) {
         let attributeString1 = NSAttributedString(string: "we all have a grate dream. then we should take much time to achieve it")
 //        attributeString1.drawInRect(rect)
-        attributeString1.drawWithRect(rect, options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil)
+//        attributeString1.drawWithRect(rect, options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil)
+        let templateImage = UIImage(named: "face0")
+//        templateImage?.drawAsPatternInRect(rect)
     }
 }
 
@@ -46,9 +49,34 @@ class ViewController: UIViewController {
         
         
         
-        let drawView = MyDrawView(frame: CGRectMake(50, 150, 200, 150))
+        let drawView = MyDrawView(frame: CGRectMake(50, 150, 200, self.view.bounds.size.height))
         
         self.view.addSubview(drawView)
+        
+        
+//        let animationImages = UIImage.animatedImageNamed("face", duration: 5)
+//        let animationImages = UIImage.animatedResizableImageNamed("face", capInsets: UIEdgeInsetsMake(10, 0, 0, 0), duration: 4)
+        let animationImages = UIImage.animatedResizableImageNamed("face", capInsets: UIEdgeInsetsMake(50, 100, 0, 0), resizingMode:.Stretch, duration: 4)
+//        let templateImage = UIImage(named: "face0")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        
+        
+        let cicolor = CIColor(red: 0.3, green: 0.5, blue: 0.3)
+        let ciimage = CIImage(color: cicolor).imageByCroppingToRect(CGRectMake(0, 0, 100, 100))
+        let image = UIImage(CIImage: ciimage)
+        
+        
+        
+        let animationImagesView = UIImageView(frame: CGRectMake(0, self.view.bounds.size.height - 355, 300, 150))
+        animationImagesView.image = image
+//        animationImagesView.contentMode = UIViewContentMode.ScaleAspectFit
+//        animationImagesView.image = templateImage
+        animationImagesView.layer.borderWidth = 2
+        animationImagesView.layer.borderColor = UIColor.redColor().CGColor
+//        animationImagesView.tintColor = UIColor.grayColor()
+        self.view.addSubview(animationImagesView)
+        
+        
+        
         
     }
 
@@ -57,6 +85,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
+        print(previousTraitCollection?.horizontalSizeClass.rawValue)
+        print(previousTraitCollection?.verticalSizeClass.rawValue)
+    }
 
     
     
